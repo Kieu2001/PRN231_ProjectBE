20646b62460975a29d6b088adaeba4360543f832
Tuan Anh <tuananh462001@gmail.com>
2023-06-01 11:03:02 +0700

    Update GenreController

--- Project_PRN231/Controllers/GenreController.cs
+++ Project_PRN231/Controllers/GenreController.cs
@@ -1,11 +1,59 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Project_PRN231.Models;
+using Project_PRN231.Repositories.IRepository;
 
 namespace Project_PRN231.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class GenreController : ControllerBase
     {
+        private readonly IGenreRepository gen;
+        private readonly IMapper _mapper;
+
+        public GenreController(IGenreRepository trackRepository, IMapper mapper)
+        {
+            gen = trackRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllGenre() 
+        { 
+            return Ok(gen.GetAllGenre());
+        }
+
+        [HttpPost]
+        public IActionResult InsertGenre(Genre genre)
+        {
+            gen.InsertGenre(genre);
+            return Ok("Inserted Successfull!!!");
+        }
+
+        [HttpPut] 
+        public IActionResult UpdateGenre(Genre genre)
+        {
+            var g = gen.GetGenreById(genre.Id);
+            if (g == null)
+            {
+                return NotFound();
+            }
+            gen.UpdateGenre(genre);
+            return Ok("Update Successfull!!!");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteGenre(Genre genre)
+        {
+            var g = gen.GetGenreById(genre.Id);
+            if (g == null)
+            {
+                return NotFound();
+            }
+            gen.DeleteGenre(genre);
+            return Ok();
+        }
     }
 }
--- Project_PRN231/DataAccess/UserManagement.cs
+++ Project_PRN231/DataAccess/UserManagement.cs
@@ -1,6 +1,122 @@
-﻿namespace Project_PRN231.DataAccess
+﻿using Project_PRN231.Models;
+
+namespace Project_PRN231.DataAccess
 {
     public class UserManagement
     {
+        private static UserManagement instance = null;
+        private static readonly object instanceLock = new object();
+        private UserManagement() { }
+        public static UserManagement Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new UserManagement();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public IEnumerable<User> GetUserList()
+        {
+            List<User> list = new List<User>();
+            try
+            {
+                var db = new PRN231_SUContext();
+                //list = db.AssignTasks.ToList();
+                list = db.Users.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return list;
+        }
+
+        public User GetUserById(int userId)
+        {
+            User? rp = null;
+            try
+            {
+                var db = new PRN231_SUContext();
+                rp = db.Users.SingleOrDefault(x => x.Id == userId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return rp;
+        }
+
+        public void AddNew(User user)
+        {
+            try
+            {
+                User rp = GetUserById(user.Id);
+                if (rp == null)
+                {
+                    var db = new PRN231_SUContext();
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("This user is already done");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Update(User user)
+        {
+            try
+            {
+                User rp = GetUserById(user.Id);
+                if (rp != null)
+                {
+                    var db = new PRN231_SUContext();
+                    db.Entry<User>(user).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("This user does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Delete(User user)
+        {
+            try
+            {
+                User rp = GetUserById(user.Id);
+                if (rp != null)
+                {
+                    var db = new PRN231_SUContext();
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("This user does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
