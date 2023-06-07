812844b3374642f648cf7633df521204950fb43d
Cuongcute <105336611+Cuongcute@users.noreply.github.com>
2023-06-07 16:41:23 +0700

    Squashed commit of the following:
    commit 8e0eb3cd715903c677acdac80022d53c75c0d33d
    Merge: fe4cd3b 146a3d4
    Author: Kieu2001 <105227181+Kieu2001@users.noreply.github.com>
    Date:   Wed Jun 7 02:20:48 2023 +0700
    
        Merge pull request #8 from Kieu2001/TuanAnh
    
        RejectTaskController
    
    commit 146a3d47acd862cc551a486fa378deafad7a4737
    Author: Tuan Anh <tuananh462001@gmail.com>
    Date:   Wed Jun 7 02:20:22 2023 +0700
    
        RejectTaskController
    
    commit fe4cd3b996cf1a2bcaca315b2ed130a00e3b8698
    Author: nhatvm10 <nhatvuminh00@gmail.com>
    Date:   Tue Jun 6 15:22:12 2023 +0700
    
        phần news home
    
    commit 85f80bcda253dbfc6f70dbcc7b7d4b496706b0bd
    Merge: 46847a5 77dbec0
    Author: Kieu2001 <105227181+Kieu2001@users.noreply.github.com>
    Date:   Tue Jun 6 03:12:08 2023 +0700
    
        Merge pull request #7 from Kieu2001/TuanAnh
    
        WritingTask
    
    commit 77dbec0912ed487791201d7908b021a6facedeb2
    Author: Tuan Anh <tuananh462001@gmail.com>
    Date:   Tue Jun 6 03:11:45 2023 +0700
    
        WritingTask
    
    commit 46847a5bf6d012ba6bcbeb441071c9ccd56f0d86
    Merge: eb0fbe9 e4521f6
    Author: Kieu2001 <105227181+Kieu2001@users.noreply.github.com>
    Date:   Tue Jun 6 01:39:34 2023 +0700
    
        Merge pull request #6 from Kieu2001/TuanAnh
    
        ChangeDb
    
    commit e4521f6c8258bf41c7bd217429bec20002d00340
    Author: Tuan Anh <tuananh462001@gmail.com>
    Date:   Tue Jun 6 01:39:03 2023 +0700
    
        ChangeDb
    
    commit eb0fbe9a10d854f89d3ce6a33400f952abe04dcb
    Merge: 913e796 adf3a32
    Author: Kieu2001 <105227181+Kieu2001@users.noreply.github.com>
    Date:   Tue Jun 6 00:50:59 2023 +0700
    
        Merge pull request #5 from Kieu2001/TuanAnh
    
        Import File
    
    commit adf3a32aeb53f016acf0419e3b27c0a2ff3c9abd
    Author: Tuan Anh <tuananh462001@gmail.com>
    Date:   Tue Jun 6 00:50:35 2023 +0700
    
        Something change
    
    commit ba33f71ee83bdb1cdc9518cea3c4bb73c1742092
    Merge: b4adff8 913e796
    Author: Tuan Anh <tuananh462001@gmail.com>
    Date:   Tue Jun 6 00:49:59 2023 +0700
    
        Merge branch 'main' into TuanAnh
    
    commit 913e79672f6e2fd8b57ad9444ccc2ec1e25019b5
    Author: Tuan Anh <tuananh462001@gmail.com>
    Date:   Tue Jun 6 00:45:46 2023 +0700
    
        Main Change
    
    commit b4adff86afda54c3aca7e339afa80fa0e78a0112
    Author: Tuan Anh <tuananh462001@gmail.com>
    Date:   Tue Jun 6 00:38:45 2023 +0700
    
        Import File
    
    commit 621a43e33f67eddacaf660eeb0e764e5de9b4869
    Merge: 3e02231 526aa02
    Author: Kieu2001 <105227181+Kieu2001@users.noreply.github.com>
    Date:   Mon Jun 5 13:59:09 2023 +0700
    
        Merge pull request #4 from Kieu2001/TuanAnh
    
        ChangeGenreManagement
    
    commit 526aa0255edbae465fb61a316788ca4f69718372
    Author: Tuan Anh <tuananh462001@gmail.com>
    Date:   Mon Jun 5 13:58:43 2023 +0700
    
        Revert "ChangeGenreManagement"
    
        This reverts commit 8df63eb455192fe52007b5432b5d5e0ac1ec055d.
    
    commit 8df63eb455192fe52007b5432b5d5e0ac1ec055d
    Author: Tuan Anh <tuananh462001@gmail.com>
    Date:   Mon Jun 5 13:55:13 2023 +0700
    
        ChangeGenreManagement
    
    commit 3e02231d4ee370f55f781303f66512a68a68299b
    Author: nhatvm10 <nhatvuminh00@gmail.com>
    Date:   Sat Jun 3 01:25:38 2023 +0700
    
        commit phan login

--- Project_PRN231/Program.cs
+++ Project_PRN231/Program.cs
@@ -59,6 +59,25 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 
+//builder.Services.AddIdentity<User, Role>()
+//    .AddEntityFrameworkStores<PRN231_SUContext>();
+//Token
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+
+        };
+    });
+
+
 builder.Services.AddCors(c =>
 {
     c.AddPolicy("AllowOrigin", option => option.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
