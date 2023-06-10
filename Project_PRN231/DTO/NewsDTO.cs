f7981809479c96d006afd03232e3fb069d925ddf
nhatvm10 <nhatvuminh00@gmail.com>
2023-06-10 11:46:31 +0700

    commit news

--- Project_PRN231/DTO/NewsDTO.cs
+++ Project_PRN231/DTO/NewsDTO.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿using Project_PRN231.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Project_PRN231.DTO
 {
-    public class News
+    public class NewsDTO
     {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? Content { get; set; }
+        public string? Image { get; set; }
+        public string? CreateBy { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string? UpdateBy { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public int? Entered { get; set; }
+        public int? GenreId { get; set; }
+
+        public virtual Genre? Genre { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual ICollection<NewsSeen> NewsSeens { get; set; }
     }
 }
--- Project_PRN231/DataAccess/NewsManagement.cs
+++ Project_PRN231/DataAccess/NewsManagement.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Project_PRN231.DTO;
+using Project_PRN231.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,70 @@ namespace Project_PRN231.DataAccess
 {
     public class NewsManagement
     {
+        private static NewsManagement instance = null;
+        private static readonly object instanceLock = new object();
+        private  NewsManagement() { }
+        public static NewsManagement Instance
+        {
+            get
+            {
+                lock(instanceLock)
+                {
+                    if(instance == null)
+                    {
+                        instance = new NewsManagement();
+                    }
+                    return instance;
+                }
+            }
+        }
+        public IEnumerable<News> GetNewsList()
+        {
+            List<News> list = new List<News>();
+            try
+            {
+                var db = new PRN231_SUContext();
+                list = db.News.ToList();
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return list;
+        }
+
+        public News GetNews(int id)
+        {
+            News? news= null;
+            try
+            {
+                var db = new PRN231_SUContext();
+                news = db.News.SingleOrDefault(x => x.Id == id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return news;
+        }
+        public IEnumerable<News> GetNewsByDate()
+        {
+            List<News> listNewsByDate = new List<News>();
+            try
+            {
+                var db = new PRN231_SUContext();
+                listNewsByDate = db.News.OrderBy(x=>x.CreateDate).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return listNewsByDate;
+        }
+        public void AddNews(News news)
+        {
+            try{ 
+                News news1 ;
+            }catch(Exception ex) { throw new Exception(ex.Message);}
+        }
     }
 }
