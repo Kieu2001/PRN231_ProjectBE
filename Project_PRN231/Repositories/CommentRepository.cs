f580e8593dcf27f202ddd681d0b250845d653cf4
Cuongcute <105336611+Cuongcute@users.noreply.github.com>
2023-06-18 21:51:52 +0700

    update

---
+++ Project_PRN231/Repositories/CommentRepository.cs
@@ -0,0 +1,27 @@
+﻿using Project_PRN231.DataAccess;
+using Project_PRN231.DTO;
+using Project_PRN231.Models;
+using Project_PRN231.Repositories.IRepository;
+
+namespace Project_PRN231.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        public void DeleteComment(int id) => CommentManagement.Instance.Delete(id);
+
+
+        public IEnumerable<CommentDTO> GetCommentBrowseList() => CommentManagement.Instance.GetCommentBrowseList();
+
+        public Comment GetCommentById(int Id) => CommentManagement.Instance.GetCommentById(Id);
+
+
+        public void InsertComment(Comment comment) => CommentManagement.Instance.AddNew(comment);
+
+
+        public void UpdateCommentFalse(int Id) => CommentManagement.Instance.UpdateFalse(Id);
+
+
+        public void UpdateCommentTrue(int Id) => CommentManagement.Instance.UpdateTrue(Id);
+
+    }
+}
---
+++ Project_PRN231/Repositories/IRepository/ICommentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Project_PRN231.DTO;
+using Project_PRN231.Models;
+
+namespace Project_PRN231.Repositories.IRepository
+{
+    public interface ICommentRepository
+    {
+        IEnumerable<CommentDTO> GetCommentBrowseList();
+        Comment GetCommentById(int Id);
+        void InsertComment(Comment comment);
+        void UpdateCommentTrue(int Id);
+        void UpdateCommentFalse(int Id);
+
+        void DeleteComment(int id);
+    }
+}
